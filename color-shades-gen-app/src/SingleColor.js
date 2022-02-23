833cb33cc9ddb2423337ea7585d548f73215cddb
Mukhtar <mukhtartanimugarba@gmail.com>
2022-02-23 15:36:44 +0100

    Update file

--- color-shades-gen-app/src/SingleColor.js
+++ color-shades-gen-app/src/SingleColor.js
@@ -3,7 +3,16 @@ import rgbToHex from './utils'
 
 const SingleColor = ({ rgb, weight, index, hexColor }) => {
   const [alert, setAlert] = useState(false);
-  
+  const bcg = rbg.join(',');
+  const hex = rgbToHex(...rbg);
+  const hexValue = `#${hexColor}`;
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [alert])
 
   return <h4>single color</h4>
 }
